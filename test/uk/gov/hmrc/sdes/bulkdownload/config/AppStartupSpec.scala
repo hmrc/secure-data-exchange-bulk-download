c25f18ca9102c0602f72e329068057bd88cc0bf7
Mihails Cirganovics <mihails.cirganovics@digital.hmrc.gov.uk>
2018-07-24 11:13:50 +0100

    SDES-1042: Service skeleton

---
+++ test/uk/gov/hmrc/sdes/bulkdownload/config/AppStartupSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.sdes.bulkdownload.config
+
+import org.mockito.Matchers.any
+import org.mockito.Mockito.{reset, verify, verifyZeroInteractions}
+import org.scalatest.mockito.MockitoSugar
+import play.api.Application
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.test.Helpers.running
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.play.test.UnitSpec
+import uk.gov.hmrc.sdes.bulkdownload.connectors.ServiceLocatorConnector
+
+class AppStartupSpec extends UnitSpec with MockitoSugar {
+
+  val mockServiceLocatorConnector: ServiceLocatorConnector = mock[ServiceLocatorConnector]
+
+  def app(registrationEnabled: Option[Boolean]): Application = {
+    reset(mockServiceLocatorConnector)
+    val config = for (flag <- registrationEnabled) yield "microservice.services.service-locator.enabled" -> flag
+    GuiceApplicationBuilder()
+      .configure(config.toSeq :_*)
+      .bindings(bind[ServiceLocatorConnector].to(mockServiceLocatorConnector))
+      .build()
+  }
+
+  "Application startup" when {
+
+    "registration is enabled" should {
+      "register the microservice in service locator" in running(app(registrationEnabled = Some(true))){
+        verify(mockServiceLocatorConnector).register(any[HeaderCarrier])
+      }
+    }
+
+    "registration is disabled" should {
+      "not register with service locator" in running(app(registrationEnabled = Some(false))){
+        verifyZeroInteractions(mockServiceLocatorConnector)
+      }
+    }
+  }
+
+}
