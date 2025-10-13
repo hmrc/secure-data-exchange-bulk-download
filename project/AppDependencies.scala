1e8e0588298c1ce06b7453a3c9b36012633953f3
RyanKelly <ryan.kelly@digital.hmrc.gov.uk>
2025-10-13 09:23:34 +0100

    SDES-8188 : Addressing Reported MDTP Violations/Recommendations

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt._
 
 object AppDependencies {
   val play                 = "play-30"
-  val bootstrapPlayVersion = "8.6.0"
+  val bootstrapPlayVersion = "10.2.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% s"bootstrap-backend-$play" % bootstrapPlayVersion
@@ -12,7 +12,6 @@ object AppDependencies {
 
   val test = Seq(
     "uk.gov.hmrc"       %% s"bootstrap-test-$play" % bootstrapPlayVersion % Test,
-    "org.pegdown"       % "pegdown"                % "1.6.0"              % Test,
     "org.playframework" %% "play-test"             % current              % Test,
     "org.mockito"       %% "mockito-scala"         % "1.17.31"            % Test
   )
